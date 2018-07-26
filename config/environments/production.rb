375023b697bd22d550bd841e9cc8302d92d0d6de
basil <basil.mawejje@gmail.com>
2018-07-26 11:47:37 +0300

    Modify production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                      587,
     domain:                 'gmail.com',
-    user_name:           ENV["GMAIL_USERNAME"],
-    password:              ENV["GMAIL_PASSWORD"],
+    user_name:           'basil.mawejje@gmail.com',
+    password:              'goodasfidem26',
     authentication:      'plain',
     enable_starttls_auto: true  }
 end
